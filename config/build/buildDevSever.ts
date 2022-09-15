8016ea95d9375f5eb40e079aef68742d7dd4e82e
Alex_ilyushin <lex-ander@mail.ru>
2022-09-15 21:07:01 +0300

    6 end

--- config/build/buildDevSever.ts
+++ config/build/buildDevSever.ts
@@ -4,6 +4,7 @@ import type {Configuration as DevServerConfiguration} from "webpack-dev-server";
 export function buildDevSever (options:BuildOptions):DevServerConfiguration{
     return {
         port:options.port,
-        open:true
+        open:true,
+        historyApiFallback:true
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Routes, Route} from "react-router-dom";
 import './index.scss'
+import {Link} from 'react-router-dom';
+import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className={'app'}>
-            text123dddd
-            <Counter/>
+            <Link to={'/'}>Гланая</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
