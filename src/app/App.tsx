c7e1d22b17a6cc025e135c72cf6c38cea35d9e4f
Alex_ilyushin <lex-ander@mail.ru>
2022-09-20 21:43:57 +0300

    11 end
    11 end

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,19 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from "react-router-dom";
+import React from 'react';
+import {Link} from "react-router-dom";
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/libs/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
-    const {theme,toggleTheme} = useTheme()
+    const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={classNames('app',{hovered:false,selectable:true},[theme])}>
+        <div className={classNames('app', {hovered: false, selectable: true}, [theme])}>
             <button onClick={toggleTheme}>toggle theme</button>
             <Link to={'/'}>Гланая</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage/>}/>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
