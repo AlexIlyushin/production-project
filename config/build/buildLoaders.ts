b1639275d3760c9b63c2835c0aeac3e8e8f0b94e
Alex_ilyushin <lex-ander@mail.ru>
2022-09-25 23:40:36 +0300

    17 opt

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -33,6 +33,27 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
             'sass-loader',
         ],
     };
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['en', 'ru'],
+                            keyAsDefaultValue: false,
+                            saveMissing: true,
+                            outputPath: 'public/locales/{{locale}}/{{ns}}.json',
+                        },
+                    ],
+                ],
+            },
+        },
+    };
 
     const
         typescriptLoader = {
@@ -43,6 +64,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
