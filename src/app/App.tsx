479b8bb3ef298cef0859caeb7804d4d2ca84a341
Alex_ilyushin <lex-ander@mail.ru>
2022-10-06 16:26:48 +0300

    1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import React, { Suspense } from 'react';
-import './styles/index.scss';
+import React, { Suspense, useState } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/libs/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
@@ -13,10 +13,14 @@ function App() {
     //     throw new Error();
     // }, []);
 
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,77 @@
+import { classNames } from 'shared/libs/classNames/classNames';
+import React, {
+    FC, useCallback, useEffect, useRef, useState,
+} from 'react';
+import { Portal } from 'shared/ui/Portal/Portal';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string
+    isOpen?: boolean
+    onClose?: () => void
+}
+
+const ANIMATION_DELAY = 300;
+
+export const Modal: FC<ModalProps> = (props) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
+    };
+
+    const closeHandler = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    }, [onClose]);
+
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandler();
+        }
+    }, [closeHandler]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    return (
+        <Portal>
+            <div className={classNames(cls.Modal, mods, [className])}>
+                <div className={cls.overlay} onClick={closeHandler}>
+                    <div
+                        className={cls.content}
+                        onClick={onContentClick}
+                    >
+                        {children}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi culpa cupiditate debitis illo in iure magni maiores molestiae molestias obcaecati officia officiis, perspiciatis, praesentium quaerat rem repudiandae sunt veniam.
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
+};
