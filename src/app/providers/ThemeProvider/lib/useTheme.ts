86ef6e533b194b9fe64ef5f82e6b277de5205780
Alex_ilyushin <lex-ander@mail.ru>
2022-10-10 13:40:40 +0300

    fix modal colors part 2

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -14,6 +14,7 @@ export const useTheme = (): useThemeResult => {
         setTheme(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
+
     return {
         theme,
         toggleTheme,
