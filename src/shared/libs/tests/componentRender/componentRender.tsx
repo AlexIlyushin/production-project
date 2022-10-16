d30b93e9edec0a83afbc34eb77530d00f6b83263
Alex_ilyushin <lex-ander@mail.ru>
2022-10-16 13:34:54 +0300

    fix problem with axios

--- src/shared/libs/tests/componentRender/componentRender.tsx
+++ src/shared/libs/tests/componentRender/componentRender.tsx
@@ -7,20 +7,23 @@ import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { DeepPartial } from '@reduxjs/toolkit';
 
 export interface componentRenderOptions {
-    route?: string
-    initialState?: DeepPartial<StateSchema>
+    route?: string;
+    initialState?: DeepPartial<StateSchema>;
 }
 
 export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
-    const { route = '/', initialState } = options;
-    render(
+    const {
+        route = '/',
+        initialState,
+    } = options;
+
+    return render(
         <StoreProvider initialState={initialState}>
             <MemoryRouter initialEntries={[route]}>
                 <I18nextProvider i18n={i18nForTests}>
                     {component}
                 </I18nextProvider>
             </MemoryRouter>
-            ,
         </StoreProvider>,
     );
 }
