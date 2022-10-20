be9bcc68db55cb92cdf014e6e28bc838bac09d57
Alex_ilyushin <lex-ander@mail.ru>
2022-10-20 20:38:12 +0300

    #35 fix store decorators

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -4,8 +4,12 @@ import { userReducer } from 'entities/User';
 import { createReducerManager } from './reducerManager';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState: StateSchema) {
+export function createReduxStore(
+    initialState: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+) {
     const rootReducer: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
         counter: counterReducer,
         user: userReducer,
     };
@@ -17,7 +21,7 @@ export function createReduxStore(initialState: StateSchema) {
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
-        // @ts-ignore
+    // @ts-ignore
     store.reducerManager = reducerManager;
 
     return store;
