e9df643abcac4b30cb197005547667d23b0ea14e
Alex_ilyushin <lex-ander@mail.ru>
2023-05-12 13:41:03 +0300

    #77. add circular dependency plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
 import { BuildOptions } from './types/config';
@@ -32,6 +33,10 @@ export function buildPlugins({
         ...(isDev ? [
             new ReactRefreshWebpackPlugin({ overlay: false }),
             new BundleAnalyzerPlugin({ openAnalyzer: false }),
+            new CircularDependencyPlugin({
+                exclude: /node_modules/,
+                failOnError: true,
+            }),
         ] : []),
         ...(isProd ? [
             new MiniCssExtractPlugin({
