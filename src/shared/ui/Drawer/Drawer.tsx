39a6de39e3c721cc27c749c4646efe8d030ab23e
Alex_ilyushin <lex-ander@mail.ru>
2023-07-25 13:37:21 +0300

    #84 add browser list

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -3,7 +3,7 @@ import React, {
 } from 'react';
 import { classNames } from '@/shared/libs/classNames/classNames';
 import { useTheme } from '@/app/providers/ThemeProvider';
-import { useAnimationsLibs } from '@/shared/libs/components/AnimationProvider';
+import { AnimationProvider, useAnimationsLibs } from '@/shared/libs/components/AnimationProvider';
 import { Overlay } from '../Overlay/Overlay';
 import cls from './Drawer.module.scss';
 import { Portal } from '../Portal/Portal';
@@ -91,11 +91,17 @@ export const DrawerContent = memo((props: DrawerProps) => {
     );
 });
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
     const { isLoaded } = useAnimationsLibs();
 
     if (!isLoaded) {
         return null;
     }
     return <DrawerContent {...props} />;
-});
+};
+
+export const Drawer = (props: DrawerProps) => (
+    <AnimationProvider>
+        <DrawerAsync {...props} />
+    </AnimationProvider>
+);
