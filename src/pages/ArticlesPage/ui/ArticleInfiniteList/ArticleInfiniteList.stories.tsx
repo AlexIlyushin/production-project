384aeddaef234f619a511bc7ff3ea64cf91fe9d6
Alex_ilyushin <lex-ander@mail.ru>
2023-09-13 21:11:44 +0300

    fix stories

--- src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.stories.tsx
@@ -6,7 +6,7 @@ import { ArticleInfiniteList } from './ArticleInfiniteList';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
-    title: 'pages/ArticlePage/ArticleInfiniteList',
+    title: 'pages/ArticlesPage/ArticleInfiniteList',
     component: ArticleInfiniteList,
     argTypes: {
         backgroundColor: { control: 'color' },
