c484d98ae1aa15cd4a35db1b79ea24ebec926807
Alex_ilyushin <lex-ander@mail.ru>
2023-09-14 09:57:03 +0300

    #99 add theme addon storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -9,6 +9,14 @@ import { Theme } from '../../src/shared/const/theme';
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#f2fcff' },
+            { name: 'dark', class: Theme.DARK, color: '#00246c' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ffbd2a' },
+        ],
+    },
     controls: {
         matchers: {
             color: /(background|color)$/i,
