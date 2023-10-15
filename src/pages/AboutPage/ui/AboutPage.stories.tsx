2615e7bbfa6a6b1fac5b15f885b9b32ebfb38f12
Alex_ilyushin <lex-ander@mail.ru>
2023-10-15 13:08:37 +0300

    #109 test ci

--- src/pages/AboutPage/ui/AboutPage.stories.tsx
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import AboutPage from './AboutPage';
 
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '@/shared/const/theme';
 
@@ -12,6 +13,7 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [StoreDecorator({})],
 } as ComponentMeta<typeof AboutPage>;
 
 const Template: ComponentStory<typeof AboutPage> = () => <AboutPage />;
