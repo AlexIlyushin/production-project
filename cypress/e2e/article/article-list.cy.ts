e13717ebe34778d559e233d279fa438bca1f8db0
Alex_ilyushin <lex-ander@mail.ru>
2023-11-09 18:07:52 +0300

    #115

--- cypress/e2e/article/article-list.cy.ts
+++ cypress/e2e/article/article-list.cy.ts
@@ -8,4 +8,18 @@ describe('Пользователь заходит на страницу стат
         cy.getByTestId('ArticleList').should('exist');
         cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
     });
+
+    it('и статьи подгружаются (на стабах)', () => {
+        cy.intercept('GET', `**/articles?*`, {
+            fixture: 'articles.json',
+        });
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+    });
+
+    it.skip('пример заскипаного теста', () => {
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+        cy.get('dddddd').should('exist');
+    });
 });
